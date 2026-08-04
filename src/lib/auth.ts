8d1b2e5b000eb88cc09fd3208dbb185362732a82
swadesh-231 <swadeshchatterjee512@gmail.com>
2026-08-04 12:40:05 +0530

    setup github secreates

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db";
+import { nextCookies } from "better-auth/next-js";
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
@@ -11,4 +12,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
   },
+  plugins: [nextCookies()],
 });
